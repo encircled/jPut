1e0d4863a6fffb5cbf65131bb500296fe597aa65
Uladzislau Kisel <uladzislau.kisel@embedit.cz>
2019-10-14 10:19:35 +0200

    Fix target test passing return value

--- jput-core/src/main/java/cz/encircled/jput/runner/InvokeMethodWithParams.kt
+++ jput-core/src/main/java/cz/encircled/jput/runner/InvokeMethodWithParams.kt
@@ -6,8 +6,8 @@ import org.junit.runners.model.FrameworkMethod
 
 class InvokeMethodWithParams(private val target: Any, val method: FrameworkMethod) : InvokeMethod(method, target) {
 
-    fun evaluateWithParams(jPut: JPut?) {
-        if (method.method.parameterCount > 0) {
+    fun evaluateWithParams(jPut: JPut?): Any? {
+        return if (method.method.parameterCount > 0) {
             method.invokeExplosively(target, jPut)
         } else {
             method.invokeExplosively(target)
